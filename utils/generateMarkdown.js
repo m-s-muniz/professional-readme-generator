346235f5afeaf6620658a60c21c6de1ef1a9b382
Michael Muniz <m.s.muniz@sbcglobal.net>
2023-03-29 14:02:36 -0500

    created the license badge ,license section, and part of license link functions.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,29 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Created a function that returns a license badge based on which license is passed in
+function renderLicenseBadge(license) {
+  if (license !== "none") {
+    return `![Github license](https://img.shields.io/badge/license-${license}-mediumblue.svg)`;
+  }
+  return "";
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// Created a function that returns the license link
+function renderLicenseLink(license) {
+  if (license !== "none") {
+    return `\n* [License](#license)\n`;
+  }
+  return "";
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+//Created a function that returns the license section of README
+function renderLicenseSection(license) {
+  if (license !== "none") {
+    return `## License
+        Licensed under the ${license} license.`;
+  }
+  return "";
+}
+
+
 
 //Created a function to generate markdown for README
 function generateMarkdown(data) {

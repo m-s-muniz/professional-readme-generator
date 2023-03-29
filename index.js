d8448ab3adbaf8bac036c3e8402e97008947e8bd
Michael Muniz <m.s.muniz@sbcglobal.net>
2023-03-29 14:32:10 -0500

    Added function to initialize app.

--- index.js
+++ index.js
@@ -58,8 +58,12 @@ const questions = [
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// Function to initialize app
+function init() {
+    inquirer.prompt(questions).then((data) => {
+
+    });
+  }
 
 // Function call to initialize app
 init();
